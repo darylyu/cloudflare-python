19e0a9444e823eea1350f6de7001c99a44f1ac9e
D. Yu <dyu@fastmail.com>
2015-05-16 18:23:25 +0800

    CLI boilerplate for cache purge

--- cloudflare/cli.py
+++ cloudflare/cli.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python2
 
+import click
 
+@click.group()
+@click.version_option(prog_name='cloudflare-python', version='0.0.1')
+def cli():
+    """cloudflare-python - CLI and object wrappers for CloudFlare's API.
 
-def main():
-    print "hello world"
+    Want to contribute to development?
+
+    Fork the repo at https://github.com/darylyu/cloudflare-python
+    """
+
+@cli.command('cache')
+@click.argument('action')
+@click.argument('zone')
+@click.option('--file', default=None)
+def cache(action, zone, file):
+    click.echo("here")
 
 if __name__ == '__main__':
-    main()
+    cli()
