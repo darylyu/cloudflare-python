b85f6055eb8d45b44bc466623bf47169b1aced54
D. Yu <dyu@fastmail.com>
2016-04-04 18:02:56 +0800

    new class ZoneSerializer

---
+++ cloudflare/serializers.py
@@ -0,0 +1,23 @@
+class ZoneSerializer(object):
+
+    def __init__(self, http_response):
+        zones = http_response.json()['result']
+        self.data = zones
+
+    def human_readable(self):
+        human_readable = ''
+        zones = self.data
+        for z in zones:
+            zone_info = '\n%s - %s - %s' % (z['id'], z['name'], z['status'])
+            human_readable += zone_info
+        return human_readable
+
+    def get_basic_info(self):
+        human_readable = ''
+        zones = self.data
+
+        # return a list of lightweight zones
+        # the lightweight zones only have the IDs and names
+        lw_zones = [{'id': x['id'], 'name': x['name']} for x in zones]
+
+        return lw_zones
