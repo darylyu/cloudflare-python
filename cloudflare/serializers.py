583f673666b12464d5ffcb0c51dbb1c555cd7b43
D. Yu <dyu@fastmail.com>
2016-04-18 23:45:24 +0800

    serializers: docstrings

--- cloudflare/serializers.py
+++ cloudflare/serializers.py
@@ -1,15 +1,32 @@
 # -*- coding: utf-8 -*-
 class ZoneSerializer(object):
+    '''
+    Helper class for serializing the JSON response
+    to something that is more suited to the calling
+    function or CLI.
+
+    Example: It doesn't make sense to show all of the zone
+    info for `cloudflare zone list all` - just the most
+    important ones. And it doesn't make sense to get as
+    much info as `cloudflare zone list all` for the other
+    commands.
+    '''
 
     def __init__(self, http_response):
+        '''
+        Constructor. Accepts the HTTP response, but doesn't
+        do any parsing. Parsing is done by the other functions.
+        '''
         zones = http_response.json()['result']
         self.data = zones
 
     def human_readable(self):
         human_readable = ''
         zones = self.data
-        for z in zones:
-            zone_info = '\n%s - %s - %s' % (z['id'], z['name'], z['status'])
+        for zone in zones:
+            zone_info = '\n%s - %s - %s' % (zone['id'],
+                                            zone['name'],
+                                            zone['status'])
             human_readable += zone_info
         return human_readable
 
